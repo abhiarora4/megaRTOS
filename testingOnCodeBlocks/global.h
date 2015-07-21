21c4739791530e19ee9fb340588b564443b9ba87
Abhishek Arora <abhiarora4@users.noreply.github.com>
2015-07-21 22:15:48 +0530

    Update global.h

--- testingOnCodeBlocks/global.h
+++ testingOnCodeBlocks/global.h
@@ -7,6 +7,8 @@
 
 struct pcb *process_control_block;
 struct pcb *current_pcb;
+struct wait_list *current_wait_list_pcb=NULL;
+struct suspend_list *current_suspend_list_pcb=NULL;
 struct wait_list *topWaitList=NULL;
 struct suspend_list *topSuspendList=NULL;
 clock_t time_init;
