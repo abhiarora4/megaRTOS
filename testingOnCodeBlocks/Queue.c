813e203d478075f8878f6132967dae5db4887355
Abhishek Arora <abhiarora4@users.noreply.github.com>
2015-07-21 22:17:17 +0530

    Create Queue.c

---
+++ testingOnCodeBlocks/Queue.c
@@ -0,0 +1,40 @@
+struct node{
+    void *data;
+    struct node *next;
+};
+
+struct mQueueHandler{
+    struct node *front;
+    struct node *back;
+};
+
+void mQueueCreate(char size){
+
+}
+
+void mQueueEnque(){
+
+}
+
+void mQueueDeque(){
+
+}
+
+void mQueuePeek(){
+
+}
+
+void mQueueDelete(){
+
+}
+
+void mQueuePut(){
+}
+
+void mQueueGet(){
+
+}
+
+void mQueueIsEmpty(){
+
+}
